cf88d6d536416ff32df4429e159106ab38d00dd2
Pablo Alvarado <palvarado@lcs.on.ca>
2024-01-30 14:57:17 -0500

    Using the new gallery item view inside a scrolling list, temporarily.

---
+++ Landmarks/Views/GalleryView.swift
@@ -0,0 +1,33 @@
+//
+//  GalleryView.swift
+//  Landmarks
+//
+//  Created by Pablo Alvarado on 30/01/24.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+        
+    var body: some View {
+        
+        NavigationStack {
+            
+            List(allLandmarks) { currentLandmark in
+                
+                NavigationLink {
+                    DetailView(item: currentLandmark)
+                } label: {
+                    GalleryItemView(item: currentLandmark)
+                }
+                
+            }
+            .navigationTitle("Landmarks")
+                        
+        }
+    }
+}
+
+#Preview {
+    GalleryView()
+}
